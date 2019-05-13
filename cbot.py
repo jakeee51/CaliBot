239eb1f8a9e97936350486cbc4b141c5d923db0b
jakeee51 <jakemorfe@gmail.com>
2019-05-13 01:33:15 -0400

    Update 0.5

--- cbot.py
+++ cbot.py
@@ -44,6 +44,19 @@ def timeAdd(hrs, mins):
 def timeDif(t1, t2):
     if t1 == t2:
         return 1;
+    h1 = int(t1[:2].strip(':'))
+    h2 = int(t2[:2].strip(':'))
+    m1 = int(t1[-2:])
+    m2 = int(t2[-2:])
+    if h1 > h2:
+        hd = h1 - h2
+    else:
+        hd = h2 - h1
+    if m1 > m2:
+        md = m1 - m2
+    else:
+        md = m2 - m1
+    return (((hd * 60) + md) * 60)
 
 client = discord.Client()
 
@@ -76,7 +89,8 @@ async def on_message(message):
             dt = timeAdd(h, m)
             await message.channel.send(":pray: Devil Fruit Spawn Time :pray: ```" + dt + " " + tz + "```")
             sec = timeDif(dt, ct)
-            await asyncio.sleep(sec)
-            await message.channel.send(message.author.mention + ":open_mouth: The devil fruit has spawned at :open_mouth: :```" + dt + " " + tz + "```:triumph:You now have 25 minutes to search!:triumph:")
+            if sec <= 5400:
+                await asyncio.sleep(sec)
+                await message.channel.send(message.author.mention + ":open_mouth: The devil fruit has spawned at :open_mouth: :```" + dt + " " + tz + "```:triumph:You now have 25 minutes to search!:triumph:")
 
 client.run('NTc2OTUyMjc0MjA3NzY4NTc2.XNd-wA.EubtjmhnTgLnDL6yBBFv4OtojeU')
