823f53f8ff9a4a6485c55f03a9aff5d851774fb3
jakeee51 <jakemorfe@gmail.com>
2019-05-13 18:17:39 -0400

    Update 6.0

--- cbot.py
+++ cbot.py
@@ -73,8 +73,7 @@ async def on_message(message):
         return 1;
 
     if message.content.startswith('/startDFC'):
-        t = time.time()
-        ct = time.strftime("%I:%M", time.localtime(t))
+##        ct = return current time as string '00:00'
         st = message.content
         lst = st.split(' ')
         if len(lst) < 3:
@@ -90,10 +89,10 @@ async def on_message(message):
             m = st[-2:]
             dt = timeAdd(h, m)
             await message.channel.send(":pray: Devil Fruit Spawn Time :pray: ```" + dt + " " + tz + "```")
-            sec = timeDif(dt, ct)
-            await message.channel.send(dt + "<-df ct->" + ct)
-            if sec <= 5400:
-                await asyncio.sleep(sec)
-                await message.channel.send(message.author.mention + ":open_mouth: The devil fruit has spawned at :open_mouth: ```" + dt + " " + tz + "```:triumph:You now have 25 minutes to search!:triumph:")
+##            sec = timeDif(dt, ct)
+##            await message.channel.send(dt + "<-df ct->" + ct)
+##            if sec <= 5400:
+##                await asyncio.sleep(sec)
+##                await message.channel.send(message.author.mention + ":open_mouth: The devil fruit has spawned at :open_mouth: ```" + dt + " " + tz + "```:triumph:You now have 25 minutes to search!:triumph:")
 
 client.run('NTc2OTUyMjc0MjA3NzY4NTc2.XNd-wA.EubtjmhnTgLnDL6yBBFv4OtojeU')
