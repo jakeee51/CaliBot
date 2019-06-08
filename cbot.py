df8fb541f02707848044295eacf0ecf1e709c0ac
David Jake Morfe <jakemorfe@gmail.com>
2019-06-07 22:18:08 -0400

    Update cbot.py

--- cbot.py
+++ cbot.py
@@ -77,21 +77,25 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
-async def on_message(message):
+async def twice_daily(message):
     if message.author == client.user:
-        return 1;
+        return -1;
     
-    '''if message.content.startswith('/hugPrescription'):
-        if "Vampy#1379" in str(message.author):
+    if message.content.startswith('/hugPrescription'):
+        if "Vampy#1379" in str(message.author) or "Cali#6919" in str(message.author):
             await message.channel.send("***I'm prescribing you 2 hugs per day...Doctor's orders.*** " + message.author.mention)
             await asyncio.sleep(3)
-            prescPeriod = 0
             while True:
                 await message.channel.send("https://cdn.discordapp.com/attachments/571528488809660476/586733560388386841/image0.gif")
                 await asyncio.sleep(43200)
                 wait message.channel.send("https://cdn.discordapp.com/attachments/571528488809660476/586733569817182208/image0.gif")
                 await asyncio.sleep(43200)
-                prescPeriod += 1'''
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return -1;
+
     if message.content.startswith('/help'):
         await message.channel.send("```/lick\n/jump\n/startDFC\n/defendme```")
     if message.content == "my guardian angel":
