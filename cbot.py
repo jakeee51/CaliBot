e885d2cfcd7476655a0e0c35460fbfad008218f1
jakeee51 <jakemorfe@gmail.com>
2019-07-11 21:57:56 -0400

    0.0.2

--- cbot.py
+++ cbot.py
@@ -23,16 +23,16 @@ bot.run('token')
 pokemonDict = ["Pikachu", "Squirtle", "Charmander", "Bulbasaur", '']
 
 class Unbuffered(object):
-   def __init__(self, stream):
-       self.stream = stream
-   def write(self, data):
-       self.stream.write(data)
-       self.stream.flush()
-   def writelines(self, datas):
-       self.stream.writelines(datas)
-       self.stream.flush()
-   def __getattr__(self, attr):
-       return getattr(self.stream, attr)
+    def __init__(self, stream):
+        self.stream = stream
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+    def writelines(self, datas):
+        self.stream.writelines(datas)
+        self.stream.flush()
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
 import sys
 sys.stdout = Unbuffered(sys.stdout)
 
